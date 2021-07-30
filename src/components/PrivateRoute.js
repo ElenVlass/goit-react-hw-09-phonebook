287de009344f5cd8c375acd399835de57d5ae9a3
Elena Vlasenko <olefelena@gmail.com>
2021-07-30 22:29:49 +0300

    clean-up code

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
 import { authSelectors } from '../redux/authorization';
 
 /**
