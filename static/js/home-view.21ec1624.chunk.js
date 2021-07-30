56c9b1ec7675a4b6261d024364b8b7c68317c924
Elena Vlasenko <olefelena@gmail.com>
2021-07-30 22:28:40 +0300

    Updates

---
+++ static/js/home-view.21ec1624.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[0],{113:function(e,n,s){},117:function(e,n,s){"use strict";s.r(n);s(0),s(113);var o=s(1);n.default=function(){return Object(o.jsxs)("div",{className:"lendosOvervay",children:[Object(o.jsx)("h1",{className:"lendosTitle",children:"keep all your contacts in the convenient phonebook"}),Object(o.jsx)("h1",{className:"lendosName",children:"Secrets Of Numbers"})]})}}}]);
+//# sourceMappingURL=home-view.21ec1624.chunk.js.map
